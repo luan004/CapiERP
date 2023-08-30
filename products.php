55e313405bb5dcde1f843e53b1d2eaf4f19c105c
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2023-08-30 17:29:19 -0300

    30-08

--- products.php
+++ products.php
@@ -12,7 +12,36 @@
 
     <!-- conteudo -->
     <div class="root">
-        
+        <div class="row">
+            <h5>Produtos</h5>
+            <button class="button button-green">
+                Criar novo produto
+            </button>
+        </div>
+        <!-- table -->
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Preço</th>
+                    <th>Quantidade</th>
+                    <th>Descrição</th>
+                    <th>Editar</th>
+                    <th>Excluir</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Produto 1</td>
+                    <td>R$ 10,00</td>
+                    <td>10</td>
+                    <td>
+                        <button class="button button-red">
+                            Excluir
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
     </div>
 
     <!-- rodape -->
