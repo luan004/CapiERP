aa4c5da2013778c3e0d9215e53e238d5be78920c
Luan Gabriel <luan.gabriel.ifpr@gmail.com>
2023-09-01 00:00:10 -0300

    01-08

--- src/js/products.js
+++ src/js/products.js
@@ -5,6 +5,17 @@ $(document).ready(function () {
     $("#footer").load("src/layout/footer.html");
 });
 
+let np = false;
+$('#novoProduto').click(function () {
+    if (np) {
+        $('#formProduto').slideUp();
+        np = false;
+    } else {
+        $('#formProduto').slideDown();
+        np = true;
+    }
+});
+
 $('#formProduto').submit(function (e) {
     e.preventDefault();
 
@@ -12,7 +23,28 @@ $('#formProduto').submit(function (e) {
         nome: $('#nome').val(),
         preco: $('#preco').val(),
         qtd: $('#qtd').val(),
-        descricao: $('#descricao').val(),
-        imagem: $('#imagem').val()
+        descricao: $('#descricao').val()
     }
+
+    // new row in table
+    const newRow = `
+    <tr>
+        <td>${produto.nome}</td>
+        <td>R$${produto.preco},00</td>
+        <td>${produto.qtd}</td>
+        <td>${produto.descricao}</td>
+        <td>
+            <button class="button button-red removeProduto">
+                Excluir
+            </button>
+        </td>
+    </tr>`;
+
+    // add new row in table
+    $('#tableProdutos tbody').append(newRow);
+});
+
+// remove product
+$('#tableProdutos').on('click', '.removeProduto', function () {
+    $(this).parent().parent().remove();
 });
